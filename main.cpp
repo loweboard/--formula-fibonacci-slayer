7938539e72de58449290ff9bbb4f53b6bf7aff26
LOWE <taro@aeroport.local>
2018-08-23 00:15:14 +0800

    first commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ int n = 20;
 int f1 =1 , f2=2;
 int main(int argc, char* argv[])
 {
-  for (int i = 1; i < n; i++, f2 += f1 = f2-f1)
-  std::cout << f1 << std::endl;
+  for (int i = 1; i < n; i++)
+  {
+    f2 += f1 = f2-f1;
+    std::cout << f1 << std::endl;
+  }
 }

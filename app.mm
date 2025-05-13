d64c0c771f7eab2f4fa8aac3afeac7d1ba94de3d
盧小龍 <pyloc@hotmail.com>
2025-05-13 17:49:52 +0800

    first
    .

--- app.mm
+++ app.mm
@@ -1,4 +1,5 @@
-/* app.cpp --formula-fibonacci-slayer 
+/*>clang app.mm -fobjc-arc -fmodules -mmacosx-version-min=10.7 -o app
+ * app.cpp --formula-fibonacci-slayer 
  * Copyright (C) 2012-2015 LOWE, SAAU-LOON <224428@gmail.com>
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -13,14 +14,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
 */ 
-#include <stdio.h>
-int n = 20;
-int f1 = 1, f2 = 2;
-void main()
+#import <Foundation/Foundation.h>
+
+void main (int argc, const char * argv[])
 {
-    for (int i = 1; i < n; i++)
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSInteger f1 = 1;
+    NSInteger f2 = 2;
+    int z = 1;
+    int x = 20;
+    typedef enum GameOption {
+        argv2 = 2,
+        argv3 = 3,
+        argv4 = 4,        
+    } GameOption;
+    GameOption option = (GameOption)argv2;
+    do
     {
         f2 += f1 = f2 - f1;
-        printf("%d", f1);
-    }
+        fprintf(stdout, "%d--", (int)f1);
+        z++;
+    } while (z <= x);
+    [pool drain];
 }

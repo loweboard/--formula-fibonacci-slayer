4800e4f1940e8c5f6f5cc7a2b41a45ac0c1e8e22
盧小龍 <pyloc@hotmail.com>
2025-05-13 18:03:39 +0800

    first
    .

--- app.mm
+++ app.mm
@@ -1,7 +1,7 @@
 >>>clang app.mm -fobjc-arc -fmodules -mmacosx-version-min=10.7 -o app
 /*
  * [BANK] app.mm
- * [ARGV] --formula-fibonacci-slayer 
+ * [ARGV] --formula-fibonacci-slayer
  * Copyright (C) 2012-2015 LOWE, SAAU-LOON <224428@gmail.com>
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -23,14 +23,14 @@ void main (int argc, const char * argv[])
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     NSInteger f1 = 1;
     NSInteger f2 = 2;
-    NSString *hr = "--------------------------------------------------------"
+    NSString *hr = "--------------------------------------------------------";
     int z = 1;
     int x = 20;
     typedef enum GameOption
     {
         argv2 = 2,
         argv3 = 3,
-        argv4 = 4,        
+        argv4 = 4,
     } GameOption;
     GameOption option = (GameOption)argv2;
     do
